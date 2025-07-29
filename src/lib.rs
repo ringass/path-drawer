9c156d01202b0d3ed804bb5f3d166795e0c2c964
ringass <murilogonzaga115@gmail.com>
2025-07-28 22:15:41 -0300

    add: path to -90 degrees and setup impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use nalgebra::Vector2;
 
 pub const DIAMETER: f32 = 90.0;
-
+const DEPTH: i32 = 0;
 pub enum LoopState {
     Ready,
     EnemiesSelection,
@@ -15,38 +15,75 @@ pub struct Environment {
 }
 pub struct Setup {
     pub enemies: Vec<Vector2<f32>>,
-    pub path: Vec<Vector2<f32>>,
+    pub paths: Vec<Vec<Vector2<f32>>>,
     pub key_points: Vec<Vector2<f32>>,
     pub env: Option<Environment>,
     pub state: LoopState,
 }
 
-pub fn build_path_plan(
+impl Setup {
+    pub fn build_min_path(&mut self) {
+        let right = build_path_plan(self.env.as_ref().unwrap(), &self.key_points, DEPTH, -1.0);
+
+        let left = build_path_plan(self.env.as_ref().unwrap(), &self.key_points, DEPTH, 1.0);
+
+        let left_len = path_length(&left);
+        let right_len = path_length(&right);
+
+        if left_len < right_len {
+            self.paths = vec![left, right];
+        } else if left_len > right_len {
+            self.paths = vec![right, left];
+        } else {
+            self.paths = vec![left];
+        }
+    }
+}
+
+fn build_path_plan(
     env: &Environment,
-    trajectory: Vec<Vector2<f32>>,
+    trajectory: &[Vector2<f32>],
     depth: i32,
+    direction: f32,
 ) -> Vec<Vector2<f32>> {
     if depth >= 6 {
-        return trajectory;
+        return trajectory.to_vec();
     }
 
     if let Some(obstacle_point) = is_obstacle(env, &trajectory) {
         let start = trajectory.first().unwrap();
         let goal = trajectory.last().unwrap();
 
-        let mut subgoal = search_point(*start, obstacle_point, -1.0);
+        let mut obstacle = obstacle_point;
+        let mut subgoal;
 
-        while is_point_obstacle(env, subgoal) {
-            subgoal = search_point(subgoal, subgoal, 1.0);
+        loop {
+            subgoal = search_point(*start, obstacle, direction);
+
+            if !is_point_obstacle(env, subgoal) {
+                break;
+            }
+
+            obstacle = subgoal;
         }
 
-        let path1 = build_path_plan(env, generate_straight(*start, subgoal), depth + 1);
-        let path2 = build_path_plan(env, generate_straight(subgoal, *goal), depth + 1);
+        let path1 = build_path_plan(
+            env,
+            &generate_straight(*start, subgoal),
+            depth + 1,
+            direction,
+        );
+        let path2 = build_path_plan(
+            env,
+            &generate_straight(subgoal, *goal),
+            depth + 1,
+            direction,
+        );
 
         return join_straight(path1, path2);
     }
 
-    trajectory
+    trajectory.to_vec()
 }
 
 fn search_point(from: Vector2<f32>, obstacle: Vector2<f32>, sign: f32) -> Vector2<f32> {
@@ -71,7 +108,7 @@ fn is_point_obstacle(env: &Environment, point: Vector2<f32>) -> bool {
     false
 }
 
-fn is_obstacle(env: &Environment, trajectory: &Vec<Vector2<f32>>) -> Option<Vector2<f32>> {
+fn is_obstacle(env: &Environment, trajectory: &[Vector2<f32>]) -> Option<Vector2<f32>> {
     if trajectory.len() < 2 {
         return None;
     }
@@ -105,4 +142,14 @@ fn join_straight(t1: Vec<Vector2<f32>>, t2: Vec<Vector2<f32>>) -> Vec<Vector2<f3
     let mut result = t1;
     result.extend_from_slice(&t2[1..]);
     result
+}
+
+fn path_length(path: &[Vector2<f32>]) -> f32 {
+    let mut length = 0.0;
+
+    for window in path.windows(2) {
+        length += (window[1] - window[0]).norm();
+    }
+
+    length
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use macroquad::prelude::*;
 use nalgebra::Vector2;
 use path::*;
 
-const DEPTH: i32 = 0;
 #[macroquad::main("STO'x pathplanning")]
 async fn main() {
     let mut setup = Setup {
         enemies: vec![],
         env: None,
-        path: vec![],
+        paths: vec![vec![], vec![]],
         key_points: vec![],
         state: LoopState::InitialSelection,
     };
@@ -41,11 +40,7 @@ async fn main() {
                             enemies: [setup.enemies[0], setup.enemies[1], setup.enemies[2]],
                         });
 
-                        setup.path = build_path_plan(
-                            setup.env.as_ref().unwrap(),
-                            setup.key_points.clone(),
-                            DEPTH,
-                        );
+                        setup.build_min_path();
 
                         setup.state = LoopState::Ready;
                     }
@@ -58,39 +53,52 @@ async fn main() {
         if is_key_pressed(KeyCode::R) {
             setup.key_points.clear();
             setup.enemies.clear();
-            setup.path.clear();
+            setup.paths = vec![vec![], vec![]];
             setup.env = None;
             setup.state = LoopState::InitialSelection;
         }
 
-        draw_path(&setup.path, &setup.enemies, &setup.key_points);
+        draw_path(&setup);
         draw_text_center(&setup.state);
 
         next_frame().await;
     }
 }
 
-fn draw_path(
-    path: &Vec<Vector2<f32>>,
-    enemies: &Vec<Vector2<f32>>,
-    key_points: &Vec<Vector2<f32>>,
-) {
-    for window in path.windows(2) {
-        let a = window[0];
-        let b = window[1];
+fn draw_path(setup: &Setup) {
+    for (i, path) in setup.paths.iter().enumerate() {
+        for window in path.windows(2) {
+            let a: nalgebra::Matrix<
+                f32,
+                nalgebra::Const<2>,
+                nalgebra::Const<1>,
+                nalgebra::ArrayStorage<f32, 2, 1>,
+            > = window[0];
+            let b: nalgebra::Matrix<
+                f32,
+                nalgebra::Const<2>,
+                nalgebra::Const<1>,
+                nalgebra::ArrayStorage<f32, 2, 1>,
+            > = window[1];
 
-        draw_line(a.x, a.y, b.x, b.y, 2.0, WHITE);
+            draw_line(a.x, a.y, b.x, b.y, 2.0, if i == 0 { WHITE } else { RED });
+        }
+
+        for point in path {
+            draw_circle(
+                point.x as f32,
+                point.y as f32,
+                5.0,
+                if i == 0 { RED } else { BLUE },
+            );
+        }
     }
 
-    for point in path {
-        draw_circle(point.x as f32, point.y as f32, 5.0, RED);
-    }
-
-    for enemy in enemies {
+    for enemy in &setup.enemies {
         draw_circle(enemy.x as f32, enemy.y as f32, 45.0, BLUE);
     }
 
-    for point in key_points {
+    for point in &setup.key_points {
         draw_circle(point.x, point.y, 6.0, GREEN);
     }
 }
